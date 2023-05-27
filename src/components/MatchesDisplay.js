f22c2ce9b96fcdd2517088fce8ef1860b69c9dd9
khushishah123 <123561588+khushishah123@users.noreply.github.com>
2023-05-28 00:57:47 +0530

    Update MatchesDisplay.js

--- src/components/MatchesDisplay.js
+++ src/components/MatchesDisplay.js
@@ -10,7 +10,7 @@ const MatchesDisplay =({matches, setClickedUser}) => {
 
     const getMatches = async () => {
         try {
-            const response = await axios.get('https://bumblebee-ihl9-khushishah123.vercel.app/users', {
+            const response = await axios.get('http://localhost:8000/users', {
                 params:{userIds: JSON.stringify(matchedUserIds)}
             })
             setMatchedProfiles(response.data)
