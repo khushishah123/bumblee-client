793febdd98ef808384ae7898aeed1ddb1886d368
khushishah123 <123561588+khushishah123@users.noreply.github.com>
2023-05-28 00:59:53 +0530

    Update Onboarding.js

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -26,7 +26,7 @@ const Onboarding = () => {
     const handleSubmit=async (e) => {
         e.preventDefault()
         try{
-            const response= await axios.put('https://bumblebee-ihl9-khushishah123.vercel.app/user',{formData} )
+            const response= await axios.put('http://localhost:8000/user',{formData} )
             const success = response.status === 200
             if(success) navigate('/dashboard')
         }catch (err){
