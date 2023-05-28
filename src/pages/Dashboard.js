6d324296822f2dd479ef56a308008580ebc0eaf3
khushishah123 <123561588+khushishah123@users.noreply.github.com>
2023-05-29 00:27:32 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from 'react'
 import ChatContainer from '../components/ChatContainer'
 import {useCookies} from 'react-cookie'
 import axios from 'axios'
+import './index.css';
+
 
 const Dashboard = () => {
     const [user, setUser] = useState(null)
